ddf3d4aea3bc8037603e036ff23245117426498f
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-17 09:15:54 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 0-safe_print_list.py
+# Oscar Bedat <3961@holbertonschool.com>
+def safe_print_list(my_list=[], x=0):
+    ret = 0
+
+    try:
+        for element in my_list:
+            if ret < x:
+                print('{}'.format(my_list[ret]), end='')
+                ret +=1
+
+        print()
+    except TypeError:
+        pass
+    finally:
+        return ret
