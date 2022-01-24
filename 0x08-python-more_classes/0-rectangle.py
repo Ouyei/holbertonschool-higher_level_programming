9d93fff0f0d79b67307b2de443893eb5a63f41c3
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-24 07:02:13 -0800

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Oscar Bedat <3961@holbertonschool.com>
+"""
+Simple rectangle : An empty class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An empty Rectangle class
+    """
+
+    pass
