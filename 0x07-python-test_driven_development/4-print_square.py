f2aa290fd6114ec11cc92ca9605784ccbda1e4f6
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-25 08:58:12 -0800

    4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 4-print_square.py
+# Oscar Bedat <3961@holbertonschool.com>
+"""This module prints a square with the character '#'
+of any positive integer size.
+Example:
+    ###
+    ###
+    ###
+    * The size must be an integer greater than 0.
+"""
+
+
+def print_square(size):
