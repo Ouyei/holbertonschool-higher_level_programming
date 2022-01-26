a21f4af2065682982585d3523adf46e4e0cc3658
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-25 16:43:27 -0800

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+# Oscar Bedat <3961@holbertonschool.com>
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
