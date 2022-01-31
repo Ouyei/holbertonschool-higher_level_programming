0d4f2a725fd83240245634d0a9fa1fa8de88fe51
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-31 12:40:26 -0800

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-lookup.py
+# Oscar Bedat <3961@holbertonschool.com>
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """A list object"""
+    return (dir(obj))
