6a1b998105eb5f614c889ea6cf780cca604b0aa6
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-02-01 11:37:18 -0800

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 2-append_write.py
+# Oscar Bedat <3961@holbertonschool.com>
+"""Defines a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    """append_write appends a string at the end of a text file (UTF8)
+    Args:
+        filename (str): Defaults to "".
+        text (str): text to add. Defaults to "".
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
