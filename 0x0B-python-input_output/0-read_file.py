a477e1b6b2f3aa9f643b972890dd7e307fee011c
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-02-01 12:13:52 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,7 @@
 
 
 def read_file(filename=""):
-    """read_file reads a text file (UTF8) and prints it 2 stdout
-    Args:
-        filename (str): content of the file. Defaults to "".
-    """
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
