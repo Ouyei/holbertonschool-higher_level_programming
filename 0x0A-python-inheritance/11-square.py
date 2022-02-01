02e761b477fd86a434827ccc1e66d9e04a270ac8
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-31 16:09:26 -0800

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,20 +6,16 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Square - Multiple inheritance
-    """
-
+    """Square -- Multiple inheritance"""
     def __init__(self, size):
-        """
-        Initialize a new square.
+        """Initialize a new square.
         Args:
-        size (int): The size of the new square.
+            size (int): The size of the new square.
         """
-        self.integer_validator(size, size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
         """print string"""
-        return '[Square]' + str(self.__size) + '/' + str(self.__size)
+        return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
