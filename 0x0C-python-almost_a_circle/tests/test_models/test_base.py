fefa1751dff012b781dee076eb11a518e0a88a96
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-02-08 05:35:26 -0800

    test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -33,3 +33,12 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(base_instance.id, 115)
         base_instance = Base(67)
         self.assertEqual(base_instance.id, 67)
+
+    def test_id_as_negative(self):
+        """
+        Test for a negative Base Class id
+        """
+        base_instance = Base(-91)
+        self.assertEqual(base_instance.id, -91)
+        base_instance = Base(-4)
+        self.assertEqual(base_instance.id, -4)
