2d73c917de26b3526abb0f007d843a35a35b7575
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-03-08 14:42:42 -0800

    fixed_values

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,6 @@
 -- Inserts a new row in the table `first_table`
 -- in database `hbtn_0c_0` in MySQL Server.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School")
+
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+GROUP BY city ORDER BY avg_temp DESC;
