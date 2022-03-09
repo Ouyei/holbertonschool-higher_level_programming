3a3b0ab1e05d6125c4362eb370a8a12d57a4f1fd
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-03-09 09:07:31 -0800

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Lists all cities with their state contained in the
+-- database `hbtn_0d_usa`.
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states
+ON cities.state_id = states.id;
