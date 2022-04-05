6cb87b3f1b274212d87a99d4e09c28c7eb760d69
Ouyei <obedat@.com>
2022-04-05 13:55:00 -0700

    tasks

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-// Write an empty class Rectangle that defines a rectangle.
-
-module.exports = class Rectangle { };
+// Write an empty class Rectangle that defines a rectangle.//
+module.exports = class Rectangle {};
