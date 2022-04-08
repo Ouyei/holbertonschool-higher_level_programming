237ab41c9c06da162028667288c113d10196097a
Ouyei <obedat@.com>
2022-04-08 11:21:22 -0700

    pycodestyle_done

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""
-This script takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
-This time the script is safe from
-MySQL injections!
-"""
+"""Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
 
 import MySQLdb
 from sys import argv
@@ -20,6 +14,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
+    # Query
     with db.cursor() as cur:
         cur.execute("""
             SELECT
@@ -36,6 +31,11 @@ if __name__ == '__main__':
 
         rows = cur.fetchall()
 
+    # Print query
     if rows is not None:
         for row in rows:
             print(row)
+
+    # Close cursor
+    cur.close()
+    db.close()
