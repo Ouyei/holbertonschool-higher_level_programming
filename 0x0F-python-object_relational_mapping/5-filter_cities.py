85cecd1a0ca3caa7779f303e42f0480a81a9076a
Ouyei <obedat@.com>
2022-04-08 11:26:59 -0700

    pycodestyle_done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
-"""
+"""Lists all cities from the database hbtn_0e_4_usa"""
 
 import MySQLdb
 from sys import argv
@@ -17,6 +13,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
+    # Query
     with db.cursor() as cur:
         cur.execute("""
             SELECT
@@ -37,5 +34,10 @@ if __name__ == '__main__':
 
         rows = cur.fetchall()
 
+    # Print query
     if rows is not None:
         print(", ".join([row[1] for row in rows]))
+
+    # Close cursor
+    cur.close()
+    db.close()
