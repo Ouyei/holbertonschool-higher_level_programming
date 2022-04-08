d4dc96412968b65f73673345940cdaa0887e3360
Ouyei <obedat@.com>
2022-04-08 11:07:31 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,9 +12,17 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
+    # Start cursor
     cur = db.cursor()
+
+    # Query
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
 
+    # Print query
     for row in rows:
         print(row)
+
+    # Close cursor
+    cur.close()
+    conn.close()
