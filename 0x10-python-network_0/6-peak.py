11d51c2ef433985d8d4e897685f9127aae266a47
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-04-22 18:56:18 +0000

    task6v2

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Finds peak in unsorted list of integers"""
 
+
 def find_peak(list_of_integers):
-    """Finds peak in unsorted list of integers"""
+"""Finds peak in unsorted list of integers"""
     if type(list_of_integers) is not list:
         return
     if len(list_of_integers) == 0:
